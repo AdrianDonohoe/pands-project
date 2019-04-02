8b31a1b5846143b85d07fb31293aa3cea54fd54a
Adrian Donohoe <allezlesblues1@gmail.com>
2019-04-02 06:30:05 -0400

    Added stacked histogram

--- iris.py
+++ iris.py
@@ -30,10 +30,9 @@ s = pd.Series(range(0,50))
 vi = virginica.set_index(s)
 ve = versicolor.set_index(s)
 
-#plot.plot(setosa['sepal_length'],'g-',vi['sepal_length'],'r-',ve['sepal_length'],'b-')
-#plot.plot(vi['sepal_length'],'y-')
+plot.plot(setosa['sepal_length'],'b-',vi['sepal_length'],'r-',ve['sepal_length'],'g-')
 
-#plot.show()
+plot.show()
 
 # adapted https://www.kaggle.com/jchen2186/machine-learning-with-iris-dataset
 plot.plot(setosa['sepal_length'].values,setosa['sepal_width'].values,'b+',vi['sepal_length'].values,vi['sepal_width'].values,'r+',ve['sepal_length'].values,ve['sepal_width'].values,'g+')
@@ -42,12 +41,12 @@ plot.plot(setosa['sepal_length'].values,setosa['petal_length'].values,'b+',vi['s
 plot.show()
 plot.plot(setosa['sepal_length'].values,setosa['petal_width'].values,'b+',vi['sepal_length'].values,vi['petal_width'].values,'r+',ve['sepal_length'].values,ve['petal_width'].values,'g+')
 plot.show()
-#plot.hist(setosa['sepal_length'],vi['sepal_length'],ve['sepal_length'])
-#plot.show()
 
-
-#plot.plot(df)
-#plot.show()
+# Adapted from https://matplotlib.org/examples/statistics/histogram_demo_multihist.html
+x_multi = [setosa['sepal_length'],vi['sepal_length'],ve['sepal_length']]
+colours = ['blue','red','green']
+plot.hist(x_multi,bins=10,stacked=True,color=colours)
+plot.show()
 
 
 #print('Setosa min sepal length is ', setosa['sepal_length'].min())
