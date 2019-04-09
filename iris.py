a99d9870b4c8ef3258a50b6f101a197dbbac97ec
Adrian Donohoe <allezlesblues1@gmail.com>
2019-04-09 11:18:10 +0100

    corrected legend box after updating pandas

--- iris.py
+++ iris.py
@@ -108,7 +108,7 @@ plot.plot(setosa['petal_width'].values,setosa['sepal_length'].values,'b+',label=
 plot.plot(vi['petal_width'].values,vi['sepal_length'].values,'r+',label='virginica')
 plot.plot(ve['petal_width'].values,ve['sepal_length'].values,'g+',label='versicolor')
 # Adapted https://stackoverflow.com/a/43439132
-plot.legend(bbox_to_anchor=(0., 1.02, 1, .2), loc="lower left", mode="expand", ncol=3)
+plot.legend(bbox_to_anchor=(0., 1.02, 1.1, .2), loc="lower left", mode="expand", ncol=3)
 
 plot.subplot(4,4,8)
 plot.plot(setosa['petal_width'].values,setosa['sepal_width'].values,'b+',vi['petal_width'].values,vi['sepal_width'].values,'r+',ve['petal_width'].values,ve['sepal_width'].values,'g+')
