e4ece34e3b87672cb2d0ffeae4b0f428239c10e8
Adrian Donohoe <allezlesblues1@gmail.com>
2019-04-15 13:52:21 +0100

    Updated seaborn styling and comments

--- seaborn_iris.py
+++ seaborn_iris.py
@@ -1,21 +1,23 @@
-# Verbatim from https://seaborn.pydata.org/examples/scatterplot_matrix.html
-# In week 12 videos we saw how some Iris plots were done in seaborn. It is much simpler code and only took a few minutes to put together. In my view the seaborn plots are visually better but seeing as I spent hours putting the matplotlib ones together myslef, they can stay in the project.
+# Adapted from https://seaborn.pydata.org/examples/scatterplot_matrix.html
+# In week 12 videos we saw how some Iris plots were done in seaborn. It is much simpler code and only took a few minutes to put together. In my view the seaborn plots are visually better.
 import seaborn as sns
 import matplotlib.pyplot as plot
 
-sns.set(style="ticks")
-dfs = sns.load_dataset("iris")
-g=sns.pairplot(dfs, hue="species",markers=["o","s","D"])
-g.fig.suptitle("Sepal/Petal width/length pair plots") # Adapted from https://stackoverflow.com/a/49594152
-plot.subplots_adjust(top=0.9) # Adapted from from https://stackoverflow.com/a/29814281
-plot.show()
+#sns.set_style("ticks")  # Set the plot style
+sns.set()  # Sets default styling
+dfs = sns.load_dataset("iris") # Load the iris dataset built into seaborn
+g=sns.pairplot(dfs, hue="species",markers=["o","s","D"]) # Make a pairplot of all columns in the dataset. The hue will be determined by species.The plot markers will be dots, squares and diamonds.
+g.fig.suptitle("Sepal/Petal width/length pair plots") # Adapted from https://stackoverflow.com/a/49594152 . Add a super-title to the figure
+plot.subplots_adjust(top=0.9) # Adapted from from https://stackoverflow.com/a/29814281 Adjusts the top of the plot to make room for the super-title.
+
+plot.show() #Show the plot
 
 
 
-f , axes = plot.subplots(nrows=2,ncols=2)  # Adpated from https://matplotlib.org/gallery/subplots_axes_and_figures/subplots_demo.html
-sns.scatterplot(x=dfs.index,y='petal_length',data=dfs,hue='species',ax=axes[0,0]) # Apdapted from https://seaborn.pydata.org/generated/seaborn.scatterplot.html
-sns.scatterplot(x=dfs.index,y='sepal_length',data=dfs,hue='species',ax=axes[0,1])
-sns.scatterplot(x=dfs.index,y='petal_width',data=dfs,hue='species',ax=axes[1,0])
-sns.scatterplot(x=dfs.index,y='sepal_width',data=dfs,hue='species',ax=axes[1,1])
-f.suptitle("Measurements of Petal/Sepal widths/lengths by species")
+f , axes = plot.subplots(nrows=2,ncols=2)  # Adpated from https://matplotlib.org/gallery/subplots_axes_and_figures/subplots_demo.html . Make a figure and axes object for subplot with 2 rows and 2 columns
+sns.scatterplot(x=dfs.index,y='petal_length',data=dfs,hue='species',ax=axes[0,0],style='species') # Apdapted from https://seaborn.pydata.org/generated/seaborn.scatterplot.html . Makes a scatter plot with x-axis (index), y-axis (petal length), hued by species in axes postion 0,0. Style/markers by species.
+sns.scatterplot(x=dfs.index,y='sepal_length',data=dfs,hue='species',ax=axes[0,1],style='species')
+sns.scatterplot(x=dfs.index,y='petal_width',data=dfs,hue='species',ax=axes[1,0],style='species')
+sns.scatterplot(x=dfs.index,y='sepal_width',data=dfs,hue='species',ax=axes[1,1],style='species')
+f.suptitle("Measurements of Petal/Sepal widths/lengths by species") # Adds a super-title to the figure
 plot.show()
